cadc11081774ee5f6582fef92677ea38979c390e
Vikram Mullick <VikramMullick@vikrams-macbook.home>
2016-06-07 21:49:59 -0500

    Added Map of Wellesley

--- WellesleyAccessibility/WellesleyAccessibility/FirstViewController.swift
+++ WellesleyAccessibility/WellesleyAccessibility/FirstViewController.swift
@@ -7,18 +7,39 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate
+{
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation=true
+       
+        self.mapView.mapType = MKMapType.SatelliteFlyover
+        let dropPin = MKPointAnnotation()
+        let wellesleyLocation = CLLocationCoordinate2DMake(42.293068, -71.308583)
+        dropPin.coordinate = wellesleyLocation
+        dropPin.title = "Wellesley"
+        let center = CLLocationCoordinate2DMake(wellesleyLocation.latitude, wellesleyLocation.longitude)
+        let region = MKCoordinateRegionMake(center, MKCoordinateSpanMake(0.01,0.01))
+        self.mapView.setRegion(region, animated:true)
+
+        mapView.addAnnotation(dropPin)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
